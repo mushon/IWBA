09c805e150379a0f595f3118d6856111256aed49
Wonyoung So <mail@wonyoung.so>
2016-10-15 15:15:57 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,18 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.consider_all_requests_local = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'iwba.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :address => 'smtp.sendgrid.net',
+    :domain => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :cram_md5
+  }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
